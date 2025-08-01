6544a4e14ed50091c85c6c32d3564fcd8ae56090
Niklas Smith <niklas_smith@hotmail.com>
2025-08-02 01:45:17 +0200

    made async function that need bearer token to post

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 const nav = document.querySelector(".nav_ul")
 const loginForm = document.querySelector("#login_form")
+const carForm = document.querySelector("#addCar_form")
+let carsApi = [] 
 
 window.onload = init; 
 
 function init() {
 
  changeNav();
-
+getCars();
  if(loginForm) {
 
     loginForm.addEventListener("submit", usersLogin)
  }
+  if(carForm) {
+
+    carForm.addEventListener("submit", addCar)
+ }
 }
+
+
 
 
 function changeNav( ){
@@ -78,7 +86,7 @@ body: JSON.stringify(user)
     })
 if(resp.ok) {
     const data = await resp.json();
-  localStorage.setItem("addcar_token", data.token);
+  localStorage.setItem("addcar_token", data.response.token);
 
   window.location.href = "cars.html";
 
@@ -96,4 +104,62 @@ if(resp.ok) {
     }
 
 }
+
+
+async function addCar(event) {
+
+        event.preventDefault();
+
+    let carBrandInput = document.getElementById("carbrand").value;
+    let carModelInput =  document.getElementById("carmodel").value;
+   let  priceInput =  document.getElementById("price").value;
+    let yearInput =  document.getElementById("year").value;
+
+    if(!carBrandInput || !carModelInput ||!priceInput ||!yearInput ) {
+       console.log("måste fylla i alla fält")
+     return
+    }
+    
+    let car = {
+      carbrand: carBrandInput,
+      carmodel: carModelInput,
+      price:priceInput,
+      year:yearInput
+
+    }
+
+const token = localStorage.getItem("addcar_token")
+
+    try{
+    const resp = await fetch ("http://127.0.0.1:3002/api/cars", {
+method : "POST",
+headers:{
+"content-type": "application/json",
+"Authorization": "Bearer " + token
+
+
+} ,
+body: JSON.stringify(car) 
+    })
+if(resp.ok) {
+    const data = await resp.json();
+
+    console.log(data);
+
+} else {
+
+    throw error;
+}
+
+ 
+
+
+    } catch (error){
+        console.log("Något blev fel" + error );
+
+    }
+    
+}
+
+
 
